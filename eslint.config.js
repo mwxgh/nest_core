1c44da1fbb1eec8ae1c022ceabedc47b8eb1ce6e
Nova Mei <mwx6996@gmail.com>
2025-02-16 14:02:54 +0700

    feat: init nest

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+const eslint = require('@eslint/js')
+const globals = require('globals')
+const tseslint = require('typescript-eslint')
+const eslintConfigPrettier = require('eslint-config-prettier')
+
+module.exports = [
+  {
+    ignores: ['**/eslint.config.js'],
+  },
+
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  eslintConfigPrettier,
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+    },
+  },
+]
